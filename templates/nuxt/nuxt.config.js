52e82f0dd1b4a9c6389d582a39dd76deebca12bc
makiten <dw@angk.org>
2018-10-31 16:40:20 -0500

    Created vue baseline template

--- templates/nuxt/nuxt.config.js
+++ templates/nuxt/nuxt.config.js
@@ -3,11 +3,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'My Nuxt App',
+    title: '<%= name %>',
     meta: [
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-      {hid: 'description', name: 'description', content: 'My Nuxt App'}
+      {hid: 'description', name: 'description', content: '<%= description %>'}
     ],
     link: [
       {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
